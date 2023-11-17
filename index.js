117dd9fa18da3dbdd1a9526e8364b8e60e2322eb
Glitch (ojvjpjgame) <none>
2023-11-17 16:44:50 +0000

    Rewound to commit 6daab69ee3adf939bee96fbd5099f5bb9ac39640 + 0 edits

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+console.log(server);
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(new URL("./public/index.html", import.meta.url).pathname);
 });
 
-server.listen(3000, () => {
+io.on("connection", function(socket) {
+  console.log("A user connected!")
+});
+
+server.listen(3000, function() {
   console.log('server running at http://localhost:3000');
 });
