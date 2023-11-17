6daab69ee3adf939bee96fbd5099f5bb9ac39640
Glitch (glitch-hello-node) <none>
2023-11-17 00:08:01 +0000

    🌹🍞 Checkpoint ./public/style.css:62935347/2579 ./public/index.js:62935347/44 ./index.js:62935347/236 ./public/index.html:62935347/22

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+console.log(server);
+
+app.use(express.static("public"));
 
 app.get('/', function(req, res) {
-  res.sendFile(new URL("./index.html", import.meta.url).pathname);
+  res.sendFile(new URL("./public/index.html", import.meta.url).pathname);
+});
+
+io.on("connection", function(socket) {
+  console.log("A user connected!")
 });
 
 server.listen(3000, function() {
