d4f533d6c317775871bfaee3f9c955a846c8947e
Glitch (ojvjpjgame) <none>
2023-11-20 21:14:13 +0000

    🚜🌱 Checkpoint ./README.md:62935347/394 ./.prettierrc:62935347/281 ./index.js:62935347/2992

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 import express from 'express';
-import { createServer } from 'node:http';
-import { Server } from "socket.io";
+import {createServer} from 'node:http';
+import {Server} from 'socket.io';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  res.sendFile(new URL("./public/index.html", import.meta.url).pathname);
+app.get('/', function (req, res) {
+    res.sendFile(new URL('./public/index.html', import.meta.url).pathname);
 });
 
-io.on("connection", function(socket) {
-  console.log("A user connected!")
-  socket.on("disconnect", function() {
-    console.log("A user disconnected!")
-  });
-  socket.on("chat message", function(msg){
-    console.log("message: " + msg);
-    io.emit("chat message", msg)
-  });
+io.on('connection', function (socket) {
+    console.log('A user connected!');
+    socket.on('disconnect', function () {
+        console.log('A user disconnected!');
+    });
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });
 
-server.listen(3000, function() {
-  console.log('server running at http://localhost:3000');
+server.listen(3000, function () {
+    console.log('server running at http://localhost:3000');
 });
