067832338520bdd42e3fc1879555a795746f183f
Glitch (ojvjpjgame) <none>
2023-11-28 20:14:34 +0000

    🚝👦 Checkpoint ./game.json:62935347/83 ./public/index.html:62935347/1147 ./public/client.js:62935347/3196

---
+++ public/client.js
@@ -0,0 +1,38 @@
+var game = {
+	mode: 'flat',
+};
+
+const socket = io();
+
+const messages = document.querySelector('#messages');
+const form = document.querySelector('#form');
+const input = document.querySelector('#input');
+const rcmenu = document.querySelectorAll('.rcmenu')[0];
+
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+	if(input.value != '') {
+		socket.emit('chat message', input.value);
+		input.value = '';
+	} else {
+		socket.emit('chat message', '👍 Like!');
+	}
+});
+
+socket.on('chat message', function (msg) {
+	var item = document.createElement('li');
+	item.innerHTML = msg;
+	messages.appendChild(item);
+});
+
+/*Edit the right click menu*/
+var handlecontextmenu = function (e) {
+	/*Stop the right click menu from working the way it usually does.*/
+	e.preventDefault();
+
+	rcmenu.style.display = 'block';
+	rcmenu.style.left = e.pageX + 'px';
+	rcmenu.style.top = e.pageY + 'px';
+};
+
+document.addEventListener('contextmenu', handlecontextmenu);
