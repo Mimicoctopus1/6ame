20ca63de7173cafa1f9557bdcd7a88af7ccb4a67
Glitch (ojvjpj) <none>
2023-12-05 21:42:06 +0000

    📅🏁 Checkpoint ./.glitch-assets:62935347/359 ./service-worker.js:62935347/590

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-/*This file is for if somebody downloads this app as a PWA which you can learn about at
-https://glitch.com/~glitch-hello-installable
-This file basically just runs everything that can only work if this app is installed as a PWA.
-*/
-const CACHE_NAME = "OJVJPJ Game"; /*The app title under the icon on your phone.*/
-
-let resourcesToCache = ["./", "./img/game.png", "./game.js", "./public/style.css"];
