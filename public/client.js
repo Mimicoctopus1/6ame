5d4c68b34359f187d1112fcb169df1ea84177e22
Glitch (ojvjpj) <none>
2023-12-21 18:49:11 +0000

    🏐🍎 Checkpoint ./public/client.js:62935347/2

--- public/client.js
+++ public/client.js
@@ -64,7 +64,7 @@ socket.on("log", function(messageToLog) {
 });
 
 socket.on("runSignUpProcedure", function(THIS_IS_A_PLACEHOLDER) {
-  print('Great. To sign up, please enter <code>signup<span class = "argument">username</span><span class = "argument">password</span></code>.');
+  print('Great. To sign up, please enter <code>signup <span class = "argument">username</span> <span class = "argument">password</span></code>.');
 });
 
 var admin = function (command, p) {
