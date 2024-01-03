8f424f4fa0ce52d727d8b1719a84ce8dda460c23
Glitch (ojvjpj) <none>
2024-01-03 21:48:29 +0000

    🎙❄️ Checkpoint ./public/client.js:62935347/65

--- public/client.js
+++ public/client.js
@@ -13,7 +13,7 @@ const clearBuzzesButton = document.querySelectorAll(".clearBuzzesButton")[0];
 
 if(localStorage.signedIntoGame != "true") { /*If you aren't already signed into the game...*/
   //messages.innerHTML += "<li>Welcome to the OJVJPJ game. To sign in, type <code>signin</code>. For help, type <code>help</code>. You can type right after the <code>&gt</code> symbol</li>";
-  messages.innerHTML += "<li>Please type the word buzz right after the arrow, then press enter. If you can't seem to type there, press tab.</li>"
+  messages.innerHTML += "<li>Please type the word 'buzz' right after the arrow, then press enter.</li>"
 } else {
   socket.emit('message', 'signin ' + localStorage.username + " " + localStorage.password);
   messages.innerHTML += "<li>Welcome! You are currently signed in as:<br>" + localStorage.username +"</li>"
@@ -33,6 +33,7 @@ if(localStorage.acceptedToS != "true") {  /*If the client has never accepted the
 });
 }
 
+input.focus();
 
 input.addEventListener('keyup', function (e) {
 	if (e.key === 'Enter' || e.keyCode === 13) {
