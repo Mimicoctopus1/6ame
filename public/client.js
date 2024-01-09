f4a47d692b32ea7bd4dd5fcd716f86626cd381fb
Glitch (ojvjpj) <none>
2024-01-09 23:02:22 +0000

    👟🐦 Checkpoint ./public/client.js:62935347/15932 ./public/record.html:62935347/4

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-var gameMode = "2D";
+var gameMode = '2D';
 /*
 text: Text adventure game.
 2D: Flat, top-down view.
@@ -8,7 +8,7 @@ blind: Text adventure, only text-to-speech reads out output and voice recognitio
 
 /*Initializing with functions from imports.*/
 
-var socket = io();/*This error may be looked past; io is imported in client.html.*/
+var socket = io(); /*This error may be looked past; io is imported in client.html.*/
 
 /*Establish HTML elements.*/
 
@@ -16,39 +16,44 @@ const messages = document.querySelector('#messages');
 const form = document.querySelector('#form');
 const input = document.querySelector('#input');
 const rcmenu = document.querySelectorAll('.rcmenu')[0];
-const ToS = document.querySelectorAll(".ToS")[0];
-const ToSCheckbox = document.querySelectorAll(".ToSCheckbox")[0];
-const continueFromToS = document.querySelectorAll(".continueFromToS")[0];
-const buzzerButton = document.querySelectorAll(".buzzerButton")[0];
-const buzzesTableBody = document.querySelectorAll(".buzzesTableBody")[0];
-const clearBuzzesButton = document.querySelectorAll(".clearBuzzesButton")[0];
-const mediaPreview = document.querySelectorAll(".mediaPreview")[0];
-const mediaPreviewDownload = document.querySelectorAll(".mediaPreviewDownload")[0];
-const mediaPreviewStart = document.querySelectorAll(".mediaPreviewStart")[0];
-const mediaPreviewStop = document.querySelectorAll(".mediaPreviewStop")[0];
+const ToS = document.querySelectorAll('.ToS')[0];
+const ToSCheckbox = document.querySelectorAll('.ToSCheckbox')[0];
+const continueFromToS = document.querySelectorAll('.continueFromToS')[0];
+const buzzerButton = document.querySelectorAll('.buzzerButton')[0];
+const buzzesTableBody = document.querySelectorAll('.buzzesTableBody')[0];
+const clearBuzzesButton = document.querySelectorAll('.clearBuzzesButton')[0];
+const mediaPreview = document.querySelectorAll('.mediaPreview')[0];
+const mediaPreviewDownload = document.querySelectorAll('.mediaPreviewDownload')[0];
+const mediaPreviewStart = document.querySelectorAll('.mediaPreviewStart')[0];
+const mediaPreviewStop = document.querySelectorAll('.mediaPreviewStop')[0];
 
 /*HTML Setup*/
 
-if(localStorage.signedIntoGame != "true") { /*If you aren't already signed into the game...*/
-  //messages.innerHTML += "<li>Welcome to the OJVJPJ game. To sign in, type <code>signin</code>. For help, type <code>help</code>. You can type right after the <code>&gt</code> symbol</li>";
-  messages.innerHTML += "<li>Please type the word 'buzz' right after the arrow, then press enter. <a class = 'buzzActivation' href = '#');\">Not working? Click here!</a></li>"
+if (localStorage.signedIntoGame != 'true') {
+	/*If you aren't already signed into the game...*/
+	//messages.innerHTML += "<li>Welcome to the OJVJPJ game. To sign in, type <code>signin</code>. For help, type <code>help</code>. You can type right after the <code>&gt</code> symbol</li>";
+	messages.innerHTML +=
+		"<li>Please type the word 'buzz' right after the arrow, then press enter. <a class = 'buzzActivation' href = '#');\">Not working? Click here!</a></li>";
 } else {
-  socket.emit('message', 'signin ' + localStorage.username + " " + localStorage.password);/*Automatically sign in*/
-  messages.innerHTML += "<li>Welcome! You are currently signed in as:<br>" + localStorage.username +"</li>"
+	socket.emit('message', 'signin ' + localStorage.username + ' ' + localStorage.password); /*Automatically sign in*/
+	messages.innerHTML += '<li>Welcome! You are currently signed in as:<br>' + localStorage.username + '</li>';
 }
 
-if(localStorage.acceptedToS != "true") {  /*If the client has never accepted the Terms of Service...*/
-  input.style.display = "none";           /*Hide the text-mode input.*/
-  ToS.style.display = "block";            /*Prompt the client to accept the Terms of Service.*/
-  continueFromToS.addEventListener("click", function() {/*When the continue button is clicked...*/
-  if(ToSCheckbox.checked) {               /*Then if the checkbox is checked...*/
-    localStorage.acceptedToS = true;      /*Save the data in localStorage.*/
-    input.style.display = "inline";        /*Re-show the input.*/
-    ToS.style.display = "none";           /*Hide the Terms of Service.*/
-  } else {                                /*If the checkbox isn't checked...*/
-    alert("You haven't clicked the checkbox yet.")
-  }
-});
+if (localStorage.acceptedToS != 'true') {
+	/*If the client has never accepted the Terms of Service...*/
+	input.style.display = 'none'; /*Hide the text-mode input.*/
+	ToS.style.display = 'block'; /*Prompt the client to accept the Terms of Service.*/
+	continueFromToS.addEventListener('click', function () {
+		/*When the continue button is clicked...*/ if (ToSCheckbox.checked) {
+			/*Then if the checkbox is checked...*/
+			localStorage.acceptedToS = true; /*Save the data in localStorage.*/
+			input.style.display = 'inline'; /*Re-show the input.*/
+			ToS.style.display = 'none'; /*Hide the Terms of Service.*/
+		} else {
+			/*If the checkbox isn't checked...*/
+			alert("You haven't clicked the checkbox yet.");
+		}
+	});
 }
 
 input.focus();
@@ -61,7 +66,29 @@ var print = function (msgToPrint) {
 	messages.appendChild(printItem);
 };
 
+/*Create these variables so that I can define them again and again and again without using the var keyword later on.*/
+var recorder;
+var stream;
 
+async function startRecording() {
+	/*Make a function that...*/
+	stream = await navigator.mediaDevices.getDisplayMedia({
+		/*This built-in function gets permission from the browser */
+		video: {mediaSource: 'screen'} /*The user must allow screen recording, not audio or camera or something.*/,
+	});
+	recorder = new MediaRecorder(stream);
+
+	const chunks = [];
+	recorder.ondataavailable = function (e) {
+		chunks.push(e.data);
+	};
+	recorder.onstop = function (e) {
+		const completeBlob = new Blob(chunks, {type: chunks[0].type});
+		mediaPreview.src = URL.createObjectURL(completeBlob);
+	};
+
+	recorder.start();
+}
 
 /*Socket event preperation*/
 
@@ -71,69 +98,77 @@ socket.on('chat', function (msg) {
 	messages.appendChild(printItem);
 });
 
-socket.on('run', function(callback) {
-  callback();
+socket.on('run', function (callback) {
+	callback();
 });
 
-socket.on("log", function(messageToLog) {
-  console.log(messageToLog);
+socket.on('log', function (messageToLog) {
+	console.log(messageToLog);
 });
 
-socket.on("runSignUpProcedure", function(usernameAndPassword) {
-  if(usernameAndPassword == undefined) {
-    print('To sign up, please enter <code>signup <span class = "argument">username</span> <span class = "argument">password</span></code>.');
-  }
+socket.on('runSignUpProcedure', function (usernameAndPassword) {
+	if (usernameAndPassword == undefined) {
+		print(
+			'To sign up, please enter <code>signup <span class = "argument">username</span> <span class = "argument">password</span></code>.',
+		);
+	}
 });
 
-socket.on("signUpProcedureUsernameTaken", function() {
-  print('Uh oh! That username is taken. Please repeat with a different username.')
+socket.on('signUpProcedureUsernameTaken', function () {
+	print('Uh oh! That username is taken. Please repeat with a different username.');
 });
 
-socket.on('usernameAndPasswordAddedToUserdata', function(usernameAndPassword){
-  print('Great! Your username and password have been added to the system.<br>Username: ' + usernameAndPassword[0] + "<br>Password: " + usernameAndPassword[1]);
-  print('To sign into your new account, please type in signin ' + usernameAndPassword[0] + " " + usernameAndPassword[1]);
+socket.on('usernameAndPasswordAddedToUserdata', function (usernameAndPassword) {
+	print(
+		'Great! Your username and password have been added to the system.<br>Username: ' +
+			usernameAndPassword[0] +
+			'<br>Password: ' +
+			usernameAndPassword[1],
+	);
+	print('To sign into your new account, please type in signin ' + usernameAndPassword[0] + ' ' + usernameAndPassword[1]);
 });
 
-socket.on('signInGranted', function(words) {
-  localStorage.username = words[0];
-  localStorage.password = words[1];
-  localStorage.signedIntoGame = "true";
+socket.on('signInGranted', function (words) {
+	localStorage.username = words[0];
+	localStorage.password = words[1];
+	localStorage.signedIntoGame = 'true';
 });
 
-socket.on('incorrectPasswordOrUsername', function() {
-  print('That password-username combination is incorrect! Please try again.')
+socket.on('incorrectPasswordOrUsername', function () {
+	print('That password-username combination is incorrect! Please try again.');
 });
 
-socket.on('buzzermode', function(adminOrNot) {
-  buzzerButton.style.display = "block";
-  if(adminOrNot) {
-    clearBuzzesButton.style.display = "block";
-    clearBuzzesButton.addEventListener("click", function() {
-      socket.emit("clearBuzzes");
-    });
-  }
-  localStorage.buzzerName = prompt("What would you like your buzzer name to be?");
-  buzzerButton.addEventListener("click", function() {
-    socket.emit("buzzDetected", new Date().getTime(), localStorage.buzzerName);
-  });
+socket.on('buzzermode', function (adminOrNot) {
+	buzzerButton.style.display = 'block';
+	if (adminOrNot) {
+		clearBuzzesButton.style.display = 'block';
+		clearBuzzesButton.addEventListener('click', function () {
+			socket.emit('clearBuzzes');
+		});
+	}
+	localStorage.buzzerName = prompt('What would you like your buzzer name to be?');
+	buzzerButton.addEventListener('click', function () {
+		socket.emit('buzzDetected', new Date().getTime(), localStorage.buzzerName);
+	});
 });
 
-socket.on('buzzesUpdate', function(array) {
-  console.log(array)
-  buzzesTableBody.innerHTML = "";
-  let addArrayToBuzzesTableBodyRep = 0;
-  while(addArrayToBuzzesTableBodyRep <= array.length) {
-    buzzesTableBody.innerHTML += "<tr><td>" + array[addArrayToBuzzesTableBodyRep][0] + "</td><td>" + array[addArrayToBuzzesTableBodyRep][1]+ "</td></tr>";
-    addArrayToBuzzesTableBodyRep += 1;
-  }
-  console.log(buzzesTableBody.innerHTML + " is the HTML");
+socket.on('buzzesUpdate', function (array) {
+	console.log(array);
+	buzzesTableBody.innerHTML = '';
+	let addArrayToBuzzesTableBodyRep = 0;
+	while (addArrayToBuzzesTableBodyRep <= array.length) {
+		buzzesTableBody.innerHTML +=
+			'<tr><td>' + array[addArrayToBuzzesTableBodyRep][0] + '</td><td>' + array[addArrayToBuzzesTableBodyRep][1] + '</td></tr>';
+		addArrayToBuzzesTableBodyRep += 1;
+	}
+	console.log(buzzesTableBody.innerHTML + ' is the HTML');
 });
 
 /*Event Listeners*/
 
 /*Edit the right click menu*/
 var handlecontextmenu = function (e) {
-  /*Stop the right click menu from working the way it usually does.*/
+	/*Stop the right click menu from working the way it usually does.*/
 	e.preventDefault();
 
 	rcmenu.style.display = 'block';
@@ -147,25 +182,25 @@ var handleInputKeyup = function (e) {
 		/*When enter is pressed...*/
 		socket.emit(
 			'message',
-			input.firstChild.textContent
+			input.firstChild.textContent,
 		); /*Send a message to the server, index.js. The program seems to always put the message in a div, so I'm selecting the 
     textContent of the firstChild (the div).*/
 		input.innerHTML = ''; /*Clear the entry area.*/
-	  let printItem = document.createElement('li');
-	  printItem.innerHTML = "------------------------------------------------NEW------------------------------------------------";
-	  messages.appendChild(printItem);
+		let printItem = document.createElement('li');
+		printItem.innerHTML = '------------------------------------------------NEW------------------------------------------------';
+		messages.appendChild(printItem);
 	}
 };
 
-var enterBuzzMode = function() {
-  socket.emit('message', 'buzz');
+var enterBuzzMode = function () {
+	socket.emit('message', 'buzz');
 };
 
-var enterFullscreen = function() {
-  socket.emit('fullscreenCheck');/*Tell the server to check if it's a good idea to fullscreen or not.*/
+var enterFullscreen = function () {
+	socket.emit('fullscreenCheck'); /*Tell the server to check if it's a good idea to fullscreen or not.*/
 };
 
 document.addEventListener('contextmenu', handlecontextmenu);
 input.addEventListener('keyup', handleInputKeyup);
-document.querySelectorAll(".buzzActivation")[0].addEventListener("click", enterBuzzMode);
+document.querySelectorAll('.buzzActivation')[0].addEventListener('click', enterBuzzMode);
 document.addEventListener('click', enterFullscreen);
