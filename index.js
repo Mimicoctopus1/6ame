d7c048178583584afbf2742701098ada0a06b993
Glitch (6ame) <none>
2024-05-30 17:11:20 +0000

    📚✍️ Checkpoint ./index.js:62935347/163

--- index.js
+++ index.js
@@ -84,11 +84,14 @@ io.on('connection', function(socket) {
 		} else if (['whisperto', 'sayto', 'talkto', 'tell', 't'].includes(cmnd)) {
 		} else if (['yell', 'y', 'scream', 'shout'].includes(cmnd)) {
 		} else if (['settings'].includes(cmnd)) {
-			if (['notifications'].includes(messageWords[1])) {
+			if(['notifications'].includes(messageWords[1])) {
 			}
 		} else if(['signin'].includes(cmnd)) {
       let user = readJSON(".data/userdata.json")[messageWords[1]];
       
+      let getAuthentication = function(authType) {
+        
+      }
       
       
       socket.emit('signInGranted', [messageWords[1], readJSON(".data/userdata.json")[messageWords[1]]["password"], messageWords[3]]);/*Send the username and PW. messageWords[3] is an optional parameter given. When the user is auto-signed in, it will put "nomessage" in it. This tells the server to, right now, tell the client not to print, "Successful sign in to <username>." since the user never typed sign in.*/
